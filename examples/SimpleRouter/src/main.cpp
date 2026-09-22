9623f36c5ac65a9d78a2202ff18dba71f8f1da0d
acherrum <acherrum@just-an-engineer.com>
2026-09-22 09:43:22 +0200

    Update SimpleRouter example to show filter usage.

--- examples/SimpleRouter/src/main.cpp
+++ examples/SimpleRouter/src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
 #include "cppflask/HttpServer.h"
+#include "cppflask/html/FilterRegistry.h"
+#include "cppflask/JsonObject.h"
 #include "Router.h"
 
 int main() {
+    cppflask::html::FilterRegistry::registerFilter("pingPong", [](cppflask::JsonObject& data) {
+        auto result = std::string{};
+        auto val = std::stoll(data.getValueAsString(""));
+        if (val % 3 == 0) result += "Ping";
+        if (val % 5 == 0) result += "Pong";
+        if (result.empty()) result = std::to_string(val);
+
+        return result;
+    });
+
     std::cout << "Creating the router." << std::endl;
     Router router{};
     std::cout << "Plugging router into server." << std::endl;
